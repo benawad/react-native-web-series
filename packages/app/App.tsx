e33308831d360f693e448aa47cfb1583a5f0db5f
Ben Awad <benawad97@gmail.com>
2019-02-17 21:35:02 -0600

    nohoist react native
    same

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -8,46 +8,6 @@
  * @format
  */
 
-import React, { useState } from "react";
-import { Button, Platform, StyleSheet, Text, View } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
-
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-      <Text style={styles.instructions}>{count}</Text>
-      <Button title="increment" onPress={() => setCount(count + 1)} />
-    </View>
-  );
-};
+import { App } from "@wow/common";
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
